155fc6a076ff9ab29a69c5dae1565fb805861e85
henek <eagluhih@gmail.com>
2019-01-06 11:54:55 +0500

    Рефакторинг

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,39 +4,24 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
     if user.present?
       can :read, Client
-      can :manage, Client if user.has_any_role?(:manager, :helper)
       can :manage, :all if user.has_role? :admin
+
       can :create, :totp unless user.otp_required_for_login
-      can %i[delete], :totp if user.otp_required_for_login
+      can :delete, :totp if user.otp_required_for_login
 
       can :create, Task
       can :read, Task, users: { id: user.id }
       can %i[destroy update add_subcontactor delete_subcontactor], Task, creator: user
-      can :manage, News
+
+      can :manage, Client if user.has_role? :manager
+      can :manage, News if user.has_role? :advertising
+    else
+      can :read, News, ['published_at < ?', Time.now] do |news|
+        news.published_at && news.published_at < Time.now
+      end
+      can :create, Client
     end
-    can :read, News, ['published_at < ?', Time.now] do |news|
-      news.published_at && news.published_at < Time.now
-    end
-    can :create, Client
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
 end
--- spec/requests/news_spec.rb
+++ spec/requests/news_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "News", type: :request do
     @news = create(:news)
     @un_published_news = create(:news, published_at: nil)
     @user = create(:user)
+    @advertising = create(:user, role: :advertising)
   end
 
 
@@ -22,7 +23,7 @@ RSpec.describe "News", type: :request do
     end
 
     it 'return 200 with created_at and updated_at' do
-      get news_path(@news.slug), headers: auth_headers(@user)
+      get news_path(@news.slug), headers: auth_headers(@advertising)
       expect(response).to have_http_status(200)
       expect(response).to match_json_schema('news/id_with_created_at')
     end
@@ -33,7 +34,7 @@ RSpec.describe "News", type: :request do
         expect(response).to have_http_status(:forbidden)
       end
       it 'Попытка получить доступ к неопубликованой новости с правами' do
-        get news_path(@un_published_news.slug), headers: auth_headers(@user)
+        get news_path(@un_published_news.slug), headers: auth_headers(@advertising)
         expect(response).to have_http_status(:ok)
       end
     end
@@ -41,7 +42,7 @@ RSpec.describe "News", type: :request do
 
   describe 'POST /news' do
     it 'return created' do
-      post news_index_path, params: attributes_for(:news), headers: auth_headers(@user)
+      post news_index_path, params: attributes_for(:news), headers: auth_headers(@advertising)
       expect(response).to have_http_status(:created)
       expect(response).to match_json_schema('news/id_with_created_at')
     end
@@ -56,7 +57,7 @@ RSpec.describe "News", type: :request do
 
   describe 'DELETE /news/:id' do
     it 'return no_content' do
-      delete news_path(@news.slug), headers: auth_headers(@user)
+      delete news_path(@news.slug), headers: auth_headers(@advertising)
       expect(response).to have_http_status(:no_content)
     end
 
@@ -70,7 +71,7 @@ RSpec.describe "News", type: :request do
 
   describe 'PUT /news/:id' do
     it 'return 200' do
-      put news_path(@news.slug), headers: auth_headers(@user), params: {title: 'new title'}
+      put news_path(@news.slug), headers: auth_headers(@advertising), params: { title: 'new title' }
       expect(response).to have_http_status(:ok)
       expect(response).to match_json_schema('news/id_with_created_at')
     end
